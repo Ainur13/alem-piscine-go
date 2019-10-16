53a5536bdb64b37e4445e186f10b9a4723f69259
Ainur13 <aynuralmurzina@gmail.com>
2019-10-16 22:27:34 +0600

    print comb 2

--- printcomb2.go
+++ printcomb2.go
@@ -11,38 +11,38 @@ func PrintComb2() {
 		for i2 := 0; i2 <= 9; i2++ {
 			for i3 := 0; i3 <= 9; i3++ {
 				for i4 := 0; i4 <= 9; i4++ {
-					if i*10+i2 < i3*10+i4{
+					if i*10+i2 < i3*10+i4 {
 						z01.PrintRune(iRune)
 						z01.PrintRune(iRune2)
 						z01.PrintRune(' ')
 						z01.PrintRune(iRune3)
 						z01.PrintRune(iRune4)
 
-						if i != 9 || i2 != 8 || i3 != 9 || i4 != 9{
+						if i != 9 || i2 != 8 || i3 != 9 || i4 != 9 {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						}
 					}
 
-					if iRune4=='9'{
+					if iRune4=='9' {
 						iRune4='0'
 					} else {
 						iRune4++
 					}
 				}
-				if iRune3=='9'{
+				if iRune3=='9' {
 					iRune3='0'
 				} else {
 					iRune3++
 				}
 			}
-			if iRune2=='9'{
+			if iRune2=='9' {
 				iRune2='0'
 			} else {
 				iRune2++
 			}
 		}
-		if iRune=='9'{
+		if iRune=='9' {
 			iRune='0'
 		} else {
 			iRune++
