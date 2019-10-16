8db85d01a0dc96a97eee3f65fd505eafeefdcf84
Ainur13 <aynuralmurzina@gmail.com>
2019-10-16 16:25:29 +0600

    pra v3

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for iRune := 'z'; iRune => 'a'; iRune-- {
+	for iRune := 'z'; iRune >= 'a'; iRune-- {
 		z01.PrintRune(iRune)
 	}
 	z01.PrintRune('\n')
