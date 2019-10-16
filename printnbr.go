25becb1abd0fb85a897683a6bbcd87ad2ff6c679
Ainur13 <aynuralmurzina@gmail.com>
2019-10-16 22:58:31 +0600

    nbr

--- printnbr.go
+++ printnbr.go
@@ -2,22 +2,16 @@ package printnbr
 
 import "github.com/01-edu/z01"
 
-func PrintReverse(n int) {
-	if n > 0 {
-		m := n % 10
-		n = n / 10
-		PrintReverse(n)
-		z01.PrintRune(rune(m + 48))
-	}
-}
-
 func PrintNbr(n int) {
 	if n < 0 {
 		z01.PrintRune('-')
 		n = n * (-1)
 	}
 
-	PrintReverse(n)
-
-	z01.PrintRune(10)
+	if n > 0 {
+		m := n % 10
+		n = n / 10
+		PrintNbr(n)
+		z01.PrintRune(rune(m + 48))
+	}
 }
