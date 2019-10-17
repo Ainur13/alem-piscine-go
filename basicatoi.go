3c8bf8aa85d1e18ef0a55b5332dc9d046b775a14
Ainur13 <you@example.com>
2019-10-17 22:43:46 +0600

    basic atoi

---
+++ basicatoi.go
@@ -0,0 +1,21 @@
+package basicatoi
+
+func BasicAtoi(s string) int {
+	runes := []rune(s)
+	l := 0
+	for i := range runes {
+		i = i
+		l++
+	}
+	res := 0
+	for _, r := range runes {
+		st := 1
+		for i := 1; i < l; i++ {
+			st *= 10
+		}
+		res = res + int(r-48) * st
+		l--
+	}
+	
+	return res
+}
