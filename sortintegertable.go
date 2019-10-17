37d089ba9ecbf3a118b2b7102ff29147e6148864
Ainur13 <you@example.com>
2019-10-17 23:33:32 +0600

    sort

---
+++ sortintegertable.go
@@ -0,0 +1,18 @@
+package sortintegertable
+
+func SortIntegerTable(table []int) {
+	l := 0
+	for i := range table {
+		i = i
+		l++
+	}
+	for i := 0; i < l; i++{
+		for j := i+1; j < l; j++ {
+			if table[i] > table[j] {
+				t := table[i]
+				table[i] = table[j]
+				table[j] = t
+			}
+		}
+	}
+}
