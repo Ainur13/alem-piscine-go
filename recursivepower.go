753ca43f52ce316b522c2e6f7734b4c8d22956f8
Ainur13 <you@example.com>
2019-10-21 14:17:30 +0600

    rec power

---
+++ recursivepower.go
@@ -0,0 +1,14 @@
+package recursivepower
+
+func RecursivePower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	}
+	if power == 1 {
+		return nb
+	}
+	if power > 1 {
+		return nb * RecursivePower(nb, power-1)
+	}
+	return 0
+}
