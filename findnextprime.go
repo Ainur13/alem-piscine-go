5d3f0c4c8a1e6b95769ca635c031ce0025356a76
Ainur13 <you@example.com>
2019-10-21 14:56:12 +0600

    fin next prime

---
+++ findnextprime.go
@@ -0,0 +1,30 @@
+package finnextprime
+
+func FindNextPrime(nb int) int {
+	if nb <= 1 {
+		return 2
+	}
+	isprime := true
+	for i := 2; i <= nb/2; i++ {
+		if nb%i == 0 {
+			isprime = false
+		}
+	}
+	if isprime == true {
+		return nb
+	}
+	
+	for j := nb; j <= nb+1000; j++ {
+		isprime = true
+		for i := 2; i <= j/2; i++ {
+			if j%i == 0 {
+				isprime = false
+				i = j/2 + 1
+			}
+		}
+		if isprime == true {
+			return j
+		}
+	}
+	return 0
+}
