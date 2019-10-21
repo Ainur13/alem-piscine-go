7a360eaba58fc4828a9a33fd90b471597f4a44b6
Ainur13 <you@example.com>
2019-10-21 13:55:27 +0600

    rec factorial

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,10 +3,10 @@ package recursivefactorial
 func RecursiveFactorial(nb int) int {
 	if nb == 1 {
 		return 1
-	} 
+	}
 	if nb > 1 {
 		nb = nb * RecursiveFactorial(nb - 1)
 		return nb
-	}	
+	}
 	return 0
 }
