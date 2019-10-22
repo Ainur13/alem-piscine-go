7ce78d5517ec100c303a986efe0bd87205babe5f
Ainur13 <you@example.com>
2019-10-22 14:05:05 +0600

    nrune

--- nrune.go
+++ nrune.go
@@ -2,5 +2,14 @@ package nrune
 
 func NRune(s string, n int) rune {
 	r := []rune(s)
-	return r[n-1]
+	l := 0
+	for i := range s {
+		i = i
+		l++
+	}
+	if l > n {
+		return r[n-1]
+	} else {
+		return 0
+	}
 }
