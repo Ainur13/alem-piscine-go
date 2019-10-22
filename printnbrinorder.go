4db0b3c274e737b6bffd7e8528f382c0ad9fe48a
Ainur13 <you@example.com>
2019-10-22 13:09:58 +0600

    in order

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -4,18 +4,18 @@ import "github.com/01-edu/z01.PrintRune"
 
 func PrintNbrInOrder(n int) {
 	var runes []rune
-	if n==0 || n<0 {
+	if n == 0 || n < 0 {
 		z01.PrintRune('0')
 	} else {
 		l := 0
 		for n > 0 {
-			rem := n%10
-			runes = append(runes,rune(rem))
-			n = n/10
+			rem := n % 10
+			runes = append(runes, rune(rem))
+			n = n / 10
 			l++
 		}
 		for i := 0; i < l; i++ {
-			for j := i+1; j < l; j++ {
+			for j := i + 1; j < l; j++ {
 				if runes[i] > runes[j] {
 					t := runes[i]
 					runes[i] = runes[j]
