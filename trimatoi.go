643e94215113acfe2e03c5cc4e325c3a0043887f
Ainur13 <you@example.com>
2019-10-22 13:42:18 +0600

    trim atoi

---
+++ trimatoi.go
@@ -0,0 +1,22 @@
+package trimatoi
+
+func TrimAtoi(s string) int {
+	c := 0
+	isneg := false
+	num := 0
+	for _,l := range s {
+		if c == 0 && l == '-' {
+			isneg = true
+		}
+		if l >= '0' && l <= '9' {
+			num *= 10
+			num += int(l - 48)
+			c++
+		}
+	}
+	if isneg {
+		return -num
+	} else {
+		return num
+	}
+}
