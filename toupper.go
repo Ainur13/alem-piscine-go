883aeb926f0ce8ba6b2f3ffa65003d5db820101f
Ainur13 <you@example.com>
2019-10-22 14:54:42 +0600

    upper

--- toupper.go
+++ toupper.go
@@ -3,7 +3,7 @@ package piscine
 func ToUpper(s string) string {
 	res := ""
 	runes := []rune(s)
-	for _,r := range runes {
+	for _, r := range runes {
 		if r >= 97 && r <= 122 {
 			res = res + string(r-32)
 		} else {
