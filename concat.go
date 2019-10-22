af5091bd4b78932fb33a513194a8a0cfeea13c0c
Ainur13 <you@example.com>
2019-10-22 22:21:33 +0600

    concat

--- concat.go
+++ concat.go
@@ -5,7 +5,7 @@ func Concat(str1 string, str2 string) string {
 	runes1 := []rune(str1)
 	for _, r := range runes1 {
 		res = append(res, r)
-	} 
+	}
 	runes2 := []rune(str2)
 	for _, r := range runes2 {
 		res = append(res, r)
