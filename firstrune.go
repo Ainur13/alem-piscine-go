f030efc1f0a33d1d2c8c2a7257bbd5fe069e46d5
Ainur13 <you@example.com>
2019-10-22 13:58:32 +0600

    first rune

--- firstrune.go
+++ firstrune.go
@@ -1,17 +1,7 @@
-package main
-
-import (
-	"github.com/01-edu/z01"
-)
+package firstrune
 
 func FirstRune(s string) rune {
 	r := []rune(s)
 	return r[0]
 }
 
-func main() {
-	z01.PrintRune(FirstRune("Hello!"))
-	z01.PrintRune(FirstRune("Salut!"))
-	z01.PrintRune(FirstRune("Ola!"))
-	z01.PrintRune('\n')
-}
