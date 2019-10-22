0ed14d4cf3f91b9870e4caca95e06ee3bef14f04
Ainur13 <you@example.com>
2019-10-22 22:22:44 +0600

    concat

--- concat.go
+++ concat.go
@@ -1,14 +1,5 @@
 package piscine
 
 func Concat(str1 string, str2 string) string {
-	var res []rune
-	runes1 := []rune(str1)
-	for _, r := range runes1 {
-		res = append(res, r)
-	}
-	runes2 := []rune(str2)
-	for _, r := range runes2 {
-		res = append(res, r)
-	}
-	return string(res)
+	return str1 + str2
 }
