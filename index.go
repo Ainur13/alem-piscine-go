5f1119429eea2cc63105e04b48ce60e8b67b7d77
Ainur13 <you@example.com>
2019-10-22 14:24:02 +0600

    index

--- index.go
+++ index.go
@@ -11,11 +11,14 @@ func Index(s string, toFind string) int {
 		i = i
 		l2++
 	}
+	if l1 == 0 || l2 == 0 {
+		return -1
+	} 
 	for i := 0; i < l1-l2; i++ {
 		if s[i] == toFind[0] {
 			eq := true
 			for j := 1; j < l2; j++ {
-				if s[i+j] != toFind[j] {
+				if i+j < l1 && s[i+j] != toFind[j] {
 					eq = false
 				}
 			}
